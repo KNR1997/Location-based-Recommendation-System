379f33703f53760f65397c1ac88d930d60d164ca
KNR1997 <kethakaranasinghe@gmail.com>
2023-08-23 02:37:11 +0530

    first commit

---
+++ src/main/java/Locationbased/Recommendation/System/Neo4j/Neo4jApplication.java
@@ -0,0 +1,13 @@
+package Locationbased.Recommendation.System.Neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Locationbased/Recommendation/System/Neo4j/Neo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package Locationbased.Recommendation.System.Neo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
