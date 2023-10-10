3e07d78b0ea9215e4e9cd18e44f0229542240b1f
KNR1997 <kethakaranasinghe@gmail.com>
2023-10-10 13:01:24 +0530

    LBRS: User rate place controller created

--- src/main/java/Locationbased/Recommendation/System/Neo4j/controllers/UserController.java
+++ src/main/java/Locationbased/Recommendation/System/Neo4j/controllers/UserController.java
@@ -1,2 +1,28 @@
-package Locationbased.Recommendation.System.Neo4j.controllers;public class PlaceController {
+package Locationbased.Recommendation.System.Neo4j.controllers;
+
+import Locationbased.Recommendation.System.Neo4j.queryResult.UserRatePlaceQueryResult;
+import Locationbased.Recommendation.System.Neo4j.requests.UserRatePlaceRequest;
+import Locationbased.Recommendation.System.Neo4j.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/ratePlace")
+    public ResponseEntity<?> ratePlace(@RequestBody UserRatePlaceRequest request) {
+        UserRatePlaceQueryResult userRatePlaceQueryResult = userService.ratePlace(request.getUserName(), request.getPlaceName(), request.getRating());
+        return new ResponseEntity<>(userRatePlaceQueryResult, HttpStatus.OK);
+    }
 }
