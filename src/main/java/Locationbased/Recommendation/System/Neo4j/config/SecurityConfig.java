27fb50b22216002dd2298d5c5c31a076b46f0a74
KNR1997 <kethakaranasinghe@gmail.com>
2023-11-13 03:24:54 +0530

    add | JWT Authentication & Authorization

--- src/main/java/Locationbased/Recommendation/System/Neo4j/config/SecurityConfig.java
+++ src/main/java/Locationbased/Recommendation/System/Neo4j/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package Locationbased.Recommendation.System.Neo4j.config;
 
+import Locationbased.Recommendation.System.Neo4j.filter.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -25,10 +29,13 @@ import java.util.Arrays;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private final JwtAuthFilter authFilter;
+
     private final UserInfoUserDetailsService userInfoUserDetailsService;
 
-    public SecurityConfig(UserInfoUserDetailsService userInfoUserDetailsService) {
+    public SecurityConfig(UserInfoUserDetailsService userInfoUserDetailsService, JwtAuthFilter jwtAuthFilter) {
         this.userInfoUserDetailsService = userInfoUserDetailsService;
+        this.authFilter = jwtAuthFilter;
     }
 
     @Bean
@@ -52,6 +59,7 @@ public class SecurityConfig {
                 )
                 .userDetailsService(userInfoUserDetailsService)
                 .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
@@ -66,6 +74,11 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
--- src/main/java/Locationbased/Recommendation/System/Neo4j/controllers/UserAuthController.java
+++ src/main/java/Locationbased/Recommendation/System/Neo4j/controllers/UserAuthController.java
@@ -1,12 +1,18 @@
 package Locationbased.Recommendation.System.Neo4j.controllers;
 
 import Locationbased.Recommendation.System.Neo4j.models.User;
+import Locationbased.Recommendation.System.Neo4j.models.dto.AuthRequest;
 import Locationbased.Recommendation.System.Neo4j.models.dto.UserDTO;
 import Locationbased.Recommendation.System.Neo4j.requests.CreateUserRequest;
+import Locationbased.Recommendation.System.Neo4j.service.JwtService;
 import Locationbased.Recommendation.System.Neo4j.service.UserService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -16,6 +22,12 @@ import java.security.Principal;
 public class UserAuthController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("/me")
     public String loggedUser(Principal principal) {
@@ -29,5 +41,15 @@ public class UserAuthController {
         UserDTO responseUser = new UserDTO(user.getUsername(), user.getRoles());
 
         return new ResponseEntity<>(responseUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
     }
 }
