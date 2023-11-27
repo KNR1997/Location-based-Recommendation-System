d339c42bbc9201da0b7088cb5dbf2a64c30408c9
KNR1997 <kethakaranasinghe@gmail.com>
2023-11-27 18:08:23 +0530

    AuthenticatedUserUtil class added

---
+++ src/main/java/Locationbased/Recommendation/System/Neo4j/config/AuthenticatedUserUtil.java
@@ -0,0 +1,17 @@
+package Locationbased.Recommendation.System.Neo4j.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuthenticatedUserUtil {
+    public static String getAuthenticatedUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
+            return userDetails.getUsername();
+        }
+
+        return null;
+    }
+}
